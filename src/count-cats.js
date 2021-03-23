7693e752109513505739892673957112148fc6c6
ybespalava <ybespalava@gmail.com>
2021-03-23 09:45:13 +0300

    feat: add function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+//* const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+//*  throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  var count = 0;
+  var Rows = [];
+  for(var i = 0; i < matrix.length; i++){
+    var Rows = matrix[i];
+    for(var j = 0; j < Rows.length; j++){
+      if(Rows[j] =="^^"){
+        count++;
+      }
+    }
+  }
+  return count;
 };
