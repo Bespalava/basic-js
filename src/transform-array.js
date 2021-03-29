042a2246a15bf0e9918c13905eeef9cdf889b43f
ybespalava <ybespalava@gmail.com>
2021-03-29 17:20:01 +0300

    feat: add function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+//*const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  //*throw new CustomError('Not implemented');
   // remove line with error and write your code here
+    if (!Array.isArray(arr)){
+        throw new Error("Error");
+    }else{
+        let res = [];
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != '--double-next' && arr[i] != '--double-prev') {
+              res.push(arr[i]);
+          } else if (arr[i] === '--discard-next') {
+              i++;
+          } else if (arr[i] === '--discard-prev' && i != 0 && arr[i - 2] != '--discard-next') {
+              res.pop();
+          } else if (arr[i] === '--double-next'&& i != arr.length - 1) {
+              res.push(arr[i + 1]);
+          } else if (arr[i] === '--double-prev' && i != 0 && arr[i - 2] != '--discard-next') {
+              res.push(arr[i - 1]);
+          } else if (arr[i] === '--double-prev' && arr[i - 2] == '--discard-next') {
+              continue;
+          }		
+      }
+      return res;
+    }
 };
