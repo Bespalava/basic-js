8c534a86847681509af152c0becd86f9e0e03818
ybespalava <ybespalava@gmail.com>
2021-03-29 10:09:07 +0300

    feat: add function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+//*const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //*throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  var Command = [];
+  if(!Array.isArray(members)){
+    return false;
+  }
+  
+  members.forEach(item => {
+    if(typeof item == 'string'){
+      item = item.replace(/\s+/g,'');
+      Command.push(item[0].toUpperCase());
+    }
+  });
+
+  var CommandStr = Command.sort().join('');
+  return CommandStr;
 };
