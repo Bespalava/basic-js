d93c71eeafae4048156ea55c407405dfafcdc9a5
ybespalava <ybespalava@gmail.com>
2021-03-29 10:33:09 +0300

    feat: add function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,8 +3,29 @@
 module.exports = function repeater(str, options) {
   //*throw new CustomError('Not implemented');
   // remove line with error and write your code here
-   let turns = 2**disksNumber - 1;
-  let seconds = Math.floor(turns / (turnsSpeed / (60*60)));
-  return {turns: turns, seconds: seconds}
+  let { 
+    repeatTimes = 1, 
+    separator = '+', 
+    addition = '', 
+    additionRepeatTimes = 1, 
+    additionSeparator = '|' 
+  } = options;
+
+  if (repeatTimes === undefined) {
+    return ''
+  }
+  
+  let strAdd = [];
+  let arrAdd = [];
+  for (let i = 0; i < repeatTimes; i++) {
+    strAdd.push(str); 
+  }
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    arrAdd.push(`${addition}`);
+  }
+
+  let add = arrAdd.join(additionSeparator);
+  strAdd = strAdd.map((e) => e + add)
+  return strAdd.join(separator);
 };
   
