2d1351eeb5f7f550865590c502ddb4b3565ab790
ybespalava <ybespalava@gmail.com>
2021-03-29 10:23:58 +0300

    feat: add function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,10 @@
-const CustomError = require("../extensions/custom-error");
+//*const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+  //*throw new CustomError('Not implemented');
   // remove line with error and write your code here
+   let turns = 2**disksNumber - 1;
+  let seconds = Math.floor(turns / (turnsSpeed / (60*60)));
+  return {turns: turns, seconds: seconds}
 };
   
